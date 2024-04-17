32e258883bbb196cc7c7a687708f639634fa7b71
Baian Liu <bliu989@gmail.com>
2024-04-17 08:53:28 -0400

    Fixed indexing issue

--- src/retriever/cluster.py
+++ src/retriever/cluster.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from langchain_core.documents import Document
 from sentence_transformers import util, SentenceTransformer
 from typing import List, Literal, Any, Dict
@@ -14,11 +15,16 @@ class ClusterRetriever(BaseRetriever):
         embedding_model: str,
         similarity_metric: Literal['cosine-similarity', 'dot-product', 'euclidian'],
         content_field: str = 'page_content',
-        clustering_model = None
+        clustering_model = None,
+        all_chunks = None
     ):
         super().__init__(documents, embedding_model, similarity_metric, content_field)
         self.clustering_model = clustering_model
-        self.name = 'custom'
+        self.name = 'cluster'
+        if all_chunks is None:
+            chunks_file = '../../data/best_buy/chunks.csv'
+            df = pd.read_csv(chunks_file)
+            self.all_chunks = df['chunks']
 
     def load_documents(self):
         self.make_embeddings()
@@ -54,7 +60,13 @@ class ClusterRetriever(BaseRetriever):
         if self.clustering_model is None:
             raise ValueError('No clustering model provided.')
         cluster_labels = self.clustering_model.labels_
-        top_clusters = set(cluster_labels[i] for i in sorted_idxs[:5])
+        top_idxs_in_all_chunks = []
+        all_chunks_list = self.all_chunks.to_list()
+        for i in sorted_idxs[:5]:
+            doc = getattr(self.documents[i], self.content_field)
+            if doc in all_chunks_list:
+                top_idxs_in_all_chunks.append(all_chunks_list.index(doc))
+        top_clusters = set(cluster_labels[j] for j in top_idxs_in_all_chunks)
         # prioritize retrieving documents from clusters present in top 5 clusters
         top_cluster_idxs = []
         other_idxs = []
